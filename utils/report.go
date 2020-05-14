7f02a00d7162270d2cccd8e3904b79053ba8bd7f
RainrainWu <s0958334772@gmail.com>
2020-05-14 15:50:34 +0800

    fix(report): fix concurrent write map issue of report

--- utils/report.go
+++ utils/report.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"sync"
+)
+
 type Metadata struct {
 	Index		string
 	Env			string
@@ -17,6 +21,7 @@ type Statistic struct {
 
 type Detail struct {
 	Content	map[string]string
+	lock	*sync.RWMutex
 }
 
 type Report struct {
@@ -24,6 +29,7 @@ type Report struct {
 	Meta	Metadata
 	Stat	Statistic
 	Dets	map[string]Detail
+	lock	*sync.RWMutex
 }
 
 type Filter struct {
@@ -34,30 +40,42 @@ func (r *Report) SetMeta(data Metadata) {
 	r.Meta = data
 	r.Dets = make(map[string]Detail)
 	r.Index = r.Meta.Index
+	r.lock = &sync.RWMutex{}
 }
 
 func (r *Report) InitDetail(name string) Detail {
+	r.lock.Lock()
 	r.Dets[name] = Detail{
 		Content: make(map[string]string),
+		lock: &sync.RWMutex{},
 	}
+	r.lock.Unlock()
 	return r.Dets[name]
 }
 
 func (d *Detail) Append(key, value string) {
+	d.lock.Lock()
 	d.Content[key] = value
+	d.lock.Unlock()
 }
 
 func (r *Report) Pass() {
+	r.lock.Lock()
 	r.Stat.Total += 1
 	r.Stat.Pass += 1
+	r.lock.Unlock()
 }
 
 func (r *Report) Warning() {
+	r.lock.Lock()
 	r.Stat.Total += 1
 	r.Stat.Warning += 1
+	r.lock.Unlock()
 }
 
 func (r *Report) Fail() {
+	r.lock.Lock()
 	r.Stat.Total += 1
 	r.Stat.Fail += 1
+	r.lock.Unlock()
 }
